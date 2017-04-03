7561c60ec1d948f3488939023ba08b357629f9b3
vagrant <vagrant@vagrant.vm>
2017-04-03 23:01:00 +0000

    returning random positive quote / negative quote, based on user's mood

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -1,6 +1,6 @@
 
 from __future__ import unicode_literals
-from model import connect_to_db, db
+from model import connect_to_db, db, Quote, Sentiment
 from server import app 
 import nltk
 from nltk.corpus import stopwords
@@ -10,18 +10,15 @@ import twitter
 import tweepy
 import os
 from datetime import datetime, timedelta
+from random import choice
+
 connect_to_db(app)
 
+
+# load trained classifier from pickle file
 classifier = pickle.load(open("naivebayes.pickle"))
 
-
-# Run new tweet(s) through trained classifier
-# tweet = "I love python-twitter!"
-# print classifier.classify(extract_features(nltk.word_tokenize(tweet)))
-
-
-# connect to Twitter API! eeks! YCDI!
-
+# connect to Twitter API
 consumer_key=os.environ["TWITTER_CONSUMER_KEY"]
 consumer_secret=os.environ["TWITTER_CONSUMER_SECRET"]
 access_token_key=os.environ["TWITTER_ACCESS_TOKEN_KEY"]
@@ -41,27 +38,46 @@ user = api.get_user('chrissyteigen')
 # reference: http://stackoverflow.com/questions/7582333/python-get-datetime-of-last-hour
 last24Hours = datetime.now() - timedelta(hours = 24)
 # print last24Hours.strftime('%Y-%m-%d %H:%M:%S')
-# >>> 2017-04-02 20:06:44
 
-
-# getting user tweets, parameters: screen_name, # tweets, include re-tweets (T/F)
+# get user tweets, parameters: screen_name, # tweets, include re-tweets (T/F)
 # Reference: https://www.quora.com/How-can-I-retrieve-from-given-users-home_timeline-with-Tweepy
-user_tweets = api.user_timeline(screen_name = 'chrissyteigen', include_rts = True, count=5)
+user_tweets = api.user_timeline(screen_name = 'YaraShahidi', include_rts = True, count=5)
+
+fresh_tweets = []
+day_old_tweets = []
+current_sentiments = []
+
+# getting most current tweets, or last 5 tweets user posted if not in last 24hrs
 for status in user_tweets:
     if status.created_at > last24Hours:
-        print status.text
-        print status.created_at
+        fresh_tweets.append(status.text)
     else:
-        print status.text
-# >>> 2017-03-29 17:54:43
+        day_old_tweets.append(status.text)
+
+# running tweet(s) through the trained classifier
+if len(fresh_tweets) > 0:
+    for tweet in fresh_tweets:
+        sentiment = classifier.classify(extract_features(nltk.word_tokenize(tweet)))
+        current_sentiments.append(sentiment)
+        
+else:
+    for tweet in day_old_tweets:
+        sentiment = classifier.classify(extract_features(nltk.word_tokenize(tweet)))
+        current_sentiments.append(sentiment)
+    
+avg_sentiment = sum(current_sentiments) / len(current_sentiments)
+
+
+if int(round(avg_sentiment)) == 1:
+    pos_quotes = db.session.query(Quote.content).filter(Quote.sentiment_id=='1').all()
+    print choice(pos_quotes)
+else:
+    neg_quotes = db.session.query(Quote.content).filter(Quote.sentiment_id=='2').all()
+    print choice(neg_quotes)
 
 
 
 
-# logic to import user's tweets - if tweet is from last 24 hours, only use that one tweet,
-#otherwise take last 5 tweets and take average of the sentiment. 
-
-# run their tweet(s) through classifier
 
 # based on classifier's output (1 or 2), query database for quotes matching that output, and where user_id 
 # has not had that quote. 
