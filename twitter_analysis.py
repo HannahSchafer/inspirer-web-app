acbf5888fdd1abed1404f918b853b69abcb124bd
vagrant <vagrant@vagrant.vm>
2017-04-03 20:17:28 +0000

    added datetime logic

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -9,6 +9,7 @@ from train_classifier import extract_features
 import twitter
 import tweepy
 import os
+from datetime import datetime, timedelta
 connect_to_db(app)
 
 classifier = pickle.load(open("naivebayes.pickle"))
@@ -34,17 +35,27 @@ auth.set_access_token(access_token_key, access_token_secret)
 api = tweepy.API(auth)
 
 user = api.get_user('chrissyteigen')
-print user.screen_name
-print user.followers_count
+# print user.screen_name
+# print user.followers_count
+
+# reference: http://stackoverflow.com/questions/7582333/python-get-datetime-of-last-hour
+last24Hours = datetime.now() - timedelta(hours = 24)
+# print last24Hours.strftime('%Y-%m-%d %H:%M:%S')
+# >>> 2017-04-02 20:06:44
 
 
 # getting user tweets, parameters: screen_name, # tweets, include re-tweets (T/F)
 # Reference: https://www.quora.com/How-can-I-retrieve-from-given-users-home_timeline-with-Tweepy
-user_tweets = api.user_timeline(screen_name = 'chrissyteigen', count = 10, include_rts = True)
-
+user_tweets = api.user_timeline(screen_name = 'chrissyteigen', include_rts = True, count=5)
 for status in user_tweets:
-    print status.text
-    print status.created_at
+    if status.created_at > last24Hours:
+        print status.text
+        print status.created_at
+    else:
+        print status.text
+# >>> 2017-03-29 17:54:43
+
+
 
 
 # logic to import user's tweets - if tweet is from last 24 hours, only use that one tweet,
