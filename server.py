368d4f79bf7eb5aeed97afb252e5947eb978bfb8
vagrant <vagrant@vagrant.vm>
2017-04-11 20:05:38 +0000

    fixed colors on bar chart

--- server.py
+++ server.py
@@ -329,33 +329,33 @@ def make_bar_chart():
     sat_pos = float(weekday_numbers["Saturday"]) / float(total_days) 
     sun_pos = float(weekday_numbers["Sunday"]) / float(total_days) 
 
-    # print mon_pos
-    # print tues_pos
-
-
+    data_to_send = [mon_pos, tues_pos, wed_pos, thurs_pos, fri_pos, sat_pos, sun_pos] 
+    
     bar_data = {
     "labels": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "datasets": [
         {
             "label": "Happiness by Day",
             "backgroundColor": [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(0, 252, 255, 0.8)',
+                'rgba(0, 252, 255, 0.8)',
+                'rgba(0, 252, 255, 0.8)',
+                'rgba(0, 252, 255, 0.8)',
+                'rgba(0, 252, 255, 0.8)',
+                'rgba(0, 252, 255, 0.8)',
+                'rgba(0, 252, 255, 0.8)'
             ],
-            "borderColor": [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            "borderWidth": 1,
-            "data": [mon_pos, tues_pos, wed_pos, thurs_pos, fri_pos, sat_pos, sun_pos],
+            # "borderColor": [
+            #     'rgba(54, 162, 235, 1)',
+            #     'rgba(54, 162, 235, 1)',
+            #     'rgba(54, 162, 235, 1)',
+            #     'rgba(54, 162, 235, 1)',
+            #     'rgba(54, 162, 235, 1)',
+            #     'rgba(54, 162, 235, 1)',
+            #     'rgba(54, 162, 235, 1)'
+            # ],
+            # "borderWidth": 1,
+            "data": data_to_send,
         }
     ]
 };
