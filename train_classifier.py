a08a1ad14dec36b7d03d5e4a901c8617c417e547
Hannah Schafer <schafer.hannah@gmail.com>
2017-04-20 17:46:19 -0700

    fixed bug in train_classifier. turned train_classifier into a function to run one time. made unique_training_words an attribute on classifier object, then unwrapped it as a global variable 'unique_training_words' for extract_features to access for ongoing use

--- train_classifier.py
+++ train_classifier.py
@@ -7,15 +7,6 @@ import pickle
 from flask import Flask
 
 
-# creating a irrelevant app instance, just to connect with db, as required by flask_sqlalchemy
-app = Flask(__name__)
-connect_to_db(app)
-
-
-#app lives in server
-# connect to db lives model
-#db lives in model
-
 #appending all words to a single list
 def get_words_in_tweets(tweets):
     all_words = []
@@ -41,60 +32,67 @@ def extract_features(tweet):
     return features
 
 
-# def stuff():
-    #Query my training tweets (as a list) from the classifier table in db
-train_tweets = (db.session.query(Classifier.tweet_content, 
-                                 Classifier.sentiment_id)
-                          .filter(Classifier.test_or_train=='train')
-                          .all())
+def train_classifier():
 
-# defining stop_words to remove from corpus
-stop_words = set(stopwords.words('english'))
+    # creating a irrelevant app instance, just to connect with db, as required by flask_sqlalchemy
+    app = Flask(__name__)
+    connect_to_db(app)
+        #Query my training tweets (as a list) from the classifier table in db
+    train_tweets = (db.session.query(Classifier.tweet_content, 
+                                     Classifier.sentiment_id)
+                              .filter(Classifier.test_or_train=='train')
+                              .all())
 
-
-# Reference Laurent Luce (http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/)
-
-# normalizing words in tweets through .lower(), exclusing all meaningless words via stop_words. 
-# word_tokenizing sentences, to split all meaningful text. Better than .split() method. 
-#appending all itemized/split tweets to a single list along with their sentiment 
-tweets = []
-for sentence, sentiment in train_tweets:
-    words_filtered = [word.lower() for word in nltk.word_tokenize(sentence) 
-                                   if word not in stop_words] 
-    tweets.append((words_filtered, sentiment))
-
-# calls get_features function on function: get_words_in_tweets, which takes the tweets as
-# an argument. This returns the keys of the features dictionary, which are all the UNIQUE words that
-# contain meaning 
-unique_training_words = get_features(get_words_in_tweets(tweets))
+    # defining stop_words to remove from corpus
+    stop_words = set(stopwords.words('english'))
 
 
-# using apply_features method 
-training_tweets = nltk.classify.apply_features(extract_features, tweets)
+    # Reference Laurent Luce (http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/)
+
+    # normalizing words in tweets through .lower(), exclusing all meaningless words via stop_words. 
+    # word_tokenizing sentences, to split all meaningful text. Better than .split() method. 
+    #appending all itemized/split tweets to a single list along with their sentiment 
+    tweets = []
+    for sentence, sentiment in train_tweets:
+        words_filtered = [word.lower() for word in nltk.word_tokenize(sentence) 
+                                       if word not in stop_words] 
+        tweets.append((words_filtered, sentiment))
+
+    # calls get_features function on function: get_words_in_tweets, which takes the tweets as
+    # an argument. This returns the keys of the features dictionary, which are all the UNIQUE words that
+    # contain meaning 
+    unique_training_words = get_features(get_words_in_tweets(tweets))
 
 
-# feeding the Naive Bayes Classifier my training set, and storing in variable 'classifier'
-classifier = nltk.NaiveBayesClassifier.train(training_tweets)
-
-# print classifier.show_most_informative_features(150)
+    # using apply_features method 
+    training_tweets = nltk.classify.apply_features(extract_features, tweets)
 
 
-# This is what will run a new tweet through the trained classifier 
-# Must give it a tweet as argument
-# print classifier.classify(extract_features(nltk.word_tokenize(tweet)))
+    # feeding the Naive Bayes Classifier my training set, and storing in variable 'classifier'
+    classifier = nltk.NaiveBayesClassifier.train(training_tweets)
 
-# saving trained classifier part 1:
-# trained classifier will be saved to the naivebayes.pickle file. 
-# wb = write in bytes
-save_classifier = open("naivebayes.pickle", "wb")
+    # print classifier.show_most_informative_features(150)
 
-# we want to dump the trained classifier. 'save_classifier' is where we are dumping it.
-pickle.dump(classifier, save_classifier)
-save_classifier.close()
+    classifier.unique_training_words = unique_training_words
+    # This is what will run a new tweet through the trained classifier 
+    # Must give it a tweet as argument
+    # print classifier.classify(extract_features(nltk.word_tokenize(tweet)))
+
+    # saving trained classifier part 1:
+    # trained classifier will be saved to the naivebayes.pickle file. 
+    # wb = write in bytes
+    save_classifier = open("naivebayes.pickle", "wb")
+
+    # we want to dump the trained classifier. 'save_classifier' is where we are dumping it.
+    pickle.dump(classifier, save_classifier)
+    save_classifier.close()
+
+
 
 # part 2 to saving the trained classifier. loading. 
 classifier_f = open("naivebayes.pickle", "rb")
 classifier = pickle.load(classifier_f)
+unique_training_words = classifier.unique_training_words
 classifier_f.close()
 
 #pickle is a way to save python objects and load those python objects
